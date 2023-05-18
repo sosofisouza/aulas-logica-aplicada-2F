a9d2c5f6c2a7f4864b804f6ce69e8275e13b82e5
rm86740 <rm86740@estudante.fieb.edu.br>
2023-05-18 08:32:33 -0300

    Criamos a classe pessoa e os objetos Adão e Eva, criamos suas formas e métodos. Por fim o método foi executado e apareceu na tela.

---
+++ src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    //Atributos da classe - forma
+    String nome;
+    String sobrenome;
+    //Método da classe - comportamento/funções/ações
+    public void falar(){
+        System.out.println("falei");
+    }
+}
