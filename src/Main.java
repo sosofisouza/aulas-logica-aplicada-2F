ca89d6e62766d26cee5d148d19228cdf94a6e3e5
rm86740 <rm86740@estudante.fieb.edu.br>
2023-05-25 07:56:00 -0300

    Criamos a classe pessoa e os objetos Adão e Eva, criamos suas formas e métodos. Por fim o método foi executado e apareceu na tela. Criação de uma nova classe pessoa qualquer, e do objeto rainha com sua forma e comportamento.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
 public class Main {
+    //onde os objetos são criados, represeenta a vida
     public static void main(String[] args) {
          //Aula de Orientação à Objetos
         //Declarar objetos
         Pessoa adao;
+
         //Instanciar objetos
         adao = new Pessoa();
         Pessoa eva = new Pessoa(); // classe objeto e construtor
+
         //Definição do formato
         adao.nome = "Adão";
         adao.sobrenome = "Silva";
         eva.nome = "Eva";
         eva.sobrenome = "Silva";
+
         //Acionar comportamento
         adao.falar();
         eva.falar();
+
+        //Criar mais objetos
+        // Classe começa co maiúsculo e objeto começa com minúsculo
+        Pessoa qualquer = new Pessoa();
+
+        //Declarar objeto
+        Pessoa rainha;
+
+        //Instanciação do objeto
+        rainha = new Pessoa();
+
+        //Definindo forma da pessoa
+        rainha.nome = "Tina";
+        rainha.sobrenome= "Tunner";
+
+        //Definição de comportamento
+        System.out.println(rainha.falar("alto"));
     }
 }
